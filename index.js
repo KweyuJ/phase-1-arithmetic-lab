fd1709e202caedc20bd21cf56cf367999f8928e1
Joy <kweyuayieko@gmail.com>
2024-03-19 19:10:05 +0300

    Finished arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
+
+const random = 6
+const num3 = 25
+const num4 = 7
+const mod = num3 % num4
+
+const max = 20
+
 
 // Write your code here
